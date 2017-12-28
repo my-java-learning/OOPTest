450345a1f25a71c180bfe74ca0f5a626309bffcf
Qsior <lukaszbojes@gmail.com>
2017-12-28 01:45:57 +0100

    Added exercise that sums up OOP

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,20 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic" , "Sausage" , 3.56 , "White");
+        //double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce" , 0.75);
+        hamburger.addHamburgerAddition3("Cheese" , 1.12);
+        System.out.println("Total burger price is " + hamburger.itemizeHamburger());
 
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon" , 5.67);
+        healthyBurger.addHamburgerAddition1("Egg" , 5.43);
+        healthyBurger.addHealthAddition2("Lentils" , 3.41);
+        System.out.println("Total Healthy Burger price is " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger();
+        deluxeBurger.itemizeHamburger();
+        System.out.println("Total Deluxe Burger price is " + deluxeBurger.itemizeHamburger());
     }
 }
